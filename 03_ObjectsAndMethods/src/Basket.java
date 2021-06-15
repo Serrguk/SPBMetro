a632f6bf80a9c20c10fb228b4fc49b3c1c28d51c
Milyukov Sergey <serrguk@gmail.om>
2021-06-15 23:26:50 +0700

    contains method added

--- 03_ObjectsAndMethods/src/Basket.java
+++ 03_ObjectsAndMethods/src/Basket.java
@@ -1,21 +1,37 @@
 public class Basket {
     private static String items = "";
+    private static int totalPrice;
 
     public static void main(String[] args) {
         add("Колбаса", 85);
         add("Молоко", 60);
         add("Масло", 120);
         print("Содержимое корзины: ");
+        System.out.println("Общая стоимость товаров: " + getTotalPrice());
         clear();
         print("Содержимое корзины: ");
+        System.out.println("Общая стоимость товаров: " + getTotalPrice());
     }
 
     public static void add(String name, int price) {
+        if (contains(name)) {
+            return;
+        }
         items = items + name + " - " + price + "\n";
+        totalPrice = totalPrice + price;
     }
 
     public static void clear() {
         items = "";
+        totalPrice = 0;
+    }
+
+    public static int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public static boolean contains(String name) {
+        return items.contains(name);
     }
 
     public static void print(String title) {
