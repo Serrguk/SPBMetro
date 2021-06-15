1324f2ca6eff296afcab285c50b212701e78b9ba
Milyukov Sergey <serrguk@gmail.om>
2021-06-15 23:12:47 +0700

    Basket's methods added

--- 03_ObjectsAndMethods/src/Basket.java
+++ 03_ObjectsAndMethods/src/Basket.java
@@ -1,3 +1,28 @@
 public class Basket {
+    private static String items = "";
 
+    public static void main(String[] args) {
+        add("Колбаса", 85);
+        add("Молоко", 60);
+        add("Масло", 120);
+        print("Содержимое корзины: ");
+        clear();
+        print("Содержимое корзины: ");
+    }
+
+    public static void add(String name, int price) {
+        items = items + name + " - " + price + "\n";
+    }
+
+    public static void clear() {
+        items = "";
+    }
+
+    public static void print(String title) {
+        System.out.println(title);
+        if (items.isEmpty())
+            System.out.println("Корзина пуста");
+        else
+            System.out.println(items);
+    }
 }
