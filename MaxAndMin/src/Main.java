3e244100cdcce34b65870246b94441f301b82c12
Milyukov Sergey <serrguk@gmail.om>
2021-06-29 00:58:57 +0700

    Homework 6.2 complete

---
+++ MaxAndMin/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Максимальное значение byte: " + Byte.MAX_VALUE + "\nМинимальное значение byte: " + Byte.MIN_VALUE + "\n");
+        System.out.println("Максимальное значение short: " + Short.MAX_VALUE + "\nМинимальное значение short: " + Short.MIN_VALUE + "\n");
+        System.out.println("Максимальное значение int: " + Integer.MAX_VALUE + "\nМинимальное значение int: " + Integer.MIN_VALUE + "\n");
+        System.out.println("Максимальное значение long: " + Long.MAX_VALUE + "\nМинимальное значение long: " + Long.MIN_VALUE + "\n");
+        System.out.println("Максимальное значение float: " + Float.MAX_VALUE + "\nМинимальное значение float: " + -Float.MAX_VALUE + "\n");
+        System.out.println("Максимальное значение double: " + Double.MAX_VALUE + "\nМинимальное значение double: " + -Double.MAX_VALUE + "\n");
+    }
+}
