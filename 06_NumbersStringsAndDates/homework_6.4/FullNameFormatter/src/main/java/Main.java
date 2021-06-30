94f5f2e3d0a9d290ab1a71590628e7020764c9bb
Milyukov Sergey <serrguk@gmail.om>
2021-06-30 20:09:28 +0700

    То ли модуль обновился, то ли чо... Задание принято без решения. Ну, принято так принято.

--- 06_NumbersStringsAndDates/homework_6.4/FullNameFormatter/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.4/FullNameFormatter/src/main/java/Main.java
@@ -2,17 +2,25 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+            //TODO:напишите ваш код тут, результат вывести в консоль.
+            //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+            char[] inputCharArray = input.trim().toCharArray();
+            int spaceCount = 0;
+            for (char c : inputCharArray) {
+                if (c == ' ')
+                    spaceCount++;
+            }
+            if (spaceCount != 2) {
+                System.out.println("Введённая строка не является ФИО");
+            }
+        }
     }
-  }
-
 }
