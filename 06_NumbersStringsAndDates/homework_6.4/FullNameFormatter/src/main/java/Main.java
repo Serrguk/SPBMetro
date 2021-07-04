65462fab3f995e634656163321dba198dbda3239
Milyukov Sergey <serrguk@gmail.om>
2021-07-04 22:05:27 +0700

    Tests complete

--- 06_NumbersStringsAndDates/homework_6.4/FullNameFormatter/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.4/FullNameFormatter/src/main/java/Main.java
@@ -10,16 +10,27 @@ public class Main {
             if (input.equals("0")) {
                 break;
             }
-            //TODO:напишите ваш код тут, результат вывести в консоль.
-            //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
-            char[] inputCharArray = input.trim().toCharArray();
-            int spaceCount = 0;
-            for (char c : inputCharArray) {
+            // Разбивка введённой строки на ФИО
+            char[] inputCharArray = input.trim().toCharArray(); //Складываем инпут в массив символов
+            int spaceCount = 0; //инициализируем счётчик пробелов
+            boolean isDigit = false; //инициализируем проверку на цифры
+            for (char c : inputCharArray) { // проходим по массиву циклом форич
                 if (c == ' ')
-                    spaceCount++;
+                    spaceCount++; //считаем пробелы
+                if (Character.isDigit(c))
+                    isDigit = true; //проверяем, есть ли цифры
             }
-            if (spaceCount != 2) {
-                System.out.println("Введённая строка не является ФИО");
+            if (spaceCount != 2 || isDigit) { //если пробелов не два или в строке есть цифры, выводим строку с ошибкой
+                System.out.println("Введенная строка не является ФИО");
+            } else {
+
+                int firstSpace = input.indexOf(' '); // определеяем, где первый пробел
+                int secondSpace = input.lastIndexOf(' '); //определяем, где второй пробел
+                String lastName = input.substring(0, firstSpace).trim(); // фамилия - с начала строки до первого пробела
+                String name = input.substring(firstSpace, secondSpace).trim(); // имя - от первого до второго пробела
+                String middleName = input.substring(secondSpace).trim(); // отчество - от второго пробела до конца строки
+
+                System.out.println("Фамилия: " + lastName + "\nИмя: " + name + "\nОтчество: " + middleName); //Вывод в нужном формате
             }
         }
     }
