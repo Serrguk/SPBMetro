8027848f7308ff88f2570314f49c1021fa188767
Milyukov Sergey <serrguk@gmail.om>
2021-07-06 00:20:40 +0700

    getPeriodFromBirthday complete

--- 07_NumbersAndDates/Periods/src/main/java/Main.java
+++ 07_NumbersAndDates/Periods/src/main/java/Main.java
@@ -1,13 +1,16 @@
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        System.out.println(getPeriodFromBirthday(LocalDate.of(1970, 1, 1)));
 
-  }
+    }
 
-  private static String getPeriodFromBirthday(LocalDate birthday) {
-    return "";
-  }
-
+    private static String getPeriodFromBirthday(LocalDate birthday) {
+        LocalDate now = LocalDate.now();
+        Period diff = Period.between(birthday, now);
+        return String.format( "%d years, %d months, %d days", diff.getYears(), diff.getMonths(), diff.getDays());
+    }
 }
