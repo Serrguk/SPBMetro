f3bc99a87bb78597b0532e42e977a98346f6dfeb
Milyukov Sergey <serrguk@gmail.om>
2021-07-06 23:54:46 +0700

    TotalEarningCalculator complete

--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -3,7 +3,6 @@ public class Main {
     public static void main(String[] args) {
 
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        //TODO: напишите ваш код, результат вывести в консоль
 
         int firstDigitVasya = text.indexOf("5");
         int vasyaIncome = Integer.parseInt(text.substring(firstDigitVasya, firstDigitVasya + 4));
