b001d565a543eb9d3618a07f5321b6d3c116918b
Milyukov Sergey <serrguk@gmail.om>
2021-07-06 23:52:42 +0700

    TotalEarningCalculator complete

--- 07_NumbersAndDates/Periods/src/main/java/Main.java
+++ 07_NumbersAndDates/Periods/src/main/java/Main.java
@@ -5,12 +5,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(getPeriodFromBirthday(LocalDate.of(1970, 1, 1)));
-
     }
 
     private static String getPeriodFromBirthday(LocalDate birthday) {
         LocalDate now = LocalDate.now();
         Period diff = Period.between(birthday, now);
-        return String.format( "%d years, %d months, %d days", diff.getYears(), diff.getMonths(), diff.getDays());
+        return String.format("%d years, %d months, %d days", diff.getYears(), diff.getMonths(), diff.getDays());
     }
 }
--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -1,9 +1,18 @@
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
-  }
+        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+        //TODO: напишите ваш код, результат вывести в консоль
 
+        int firstDigitVasya = text.indexOf("5");
+        int vasyaIncome = Integer.parseInt(text.substring(firstDigitVasya, firstDigitVasya + 4));
+
+        int firstDigitPetya = text.indexOf("7");
+        int petyaIncome = Integer.parseInt(text.substring(firstDigitPetya, firstDigitPetya + 4));
+
+        int firstDigitMasha = text.lastIndexOf("3");
+        int mashaIncome = Integer.parseInt(text.substring(firstDigitMasha, firstDigitMasha + 5));
+        System.out.println(vasyaIncome + petyaIncome + mashaIncome);
+    }
 }
