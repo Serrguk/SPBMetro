f4779823e996f76f7911be747828e787c329d687
Milyukov Sergey <serrguk@gmail.om>
2021-07-07 00:54:42 +0700

    Tests complete

--- 08_Strings/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,12 +1,27 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
   public static void main(String[] args) {
 
+    String text = "Вася заработал 5000 рублей, Петя - 7899 рубля, а Маша - 30000 рублей";
+    System.out.println(calculateSalarySum(text));
   }
 
   public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
+    String regex = "[0-9]+";
+
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(text);
+
+    int sum = 0;
+    while (matcher.find()) {
+      int start = matcher.start();
+      int end = matcher.end();
+      sum = sum + Integer.parseInt(text.substring(start, end));
+    }
+    return sum;
   }
 
 }
