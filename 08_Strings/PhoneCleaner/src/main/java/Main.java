b9d40490dcfa92e1181375af298f400bd7fcc116
Milyukov Sergey <serrguk@gmail.om>
2021-07-13 00:42:06 +0700

    PhoneCleaner complete

--- 08_Strings/PhoneCleaner/src/main/java/Main.java
+++ 08_Strings/PhoneCleaner/src/main/java/Main.java
@@ -1,17 +1,32 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+
+            String regex = "[^0-9]";
+
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(input);
+
+            String result = matcher.replaceAll("");
+
+            if (result.length() == 11 && result.charAt(0) == '7') System.out.println(result);
+            else if (result.length() == 11 && result.charAt(0) == '8')
+                System.out.println("7".concat(result.substring(1)));
+            else if (result.length() == 10)
+                System.out.println("7".concat(result));
+            else System.out.println("Неверный формат номера");
+        }
     }
-  }
 
 }
