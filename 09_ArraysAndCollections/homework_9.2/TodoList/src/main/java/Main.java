131a468c02c7491901809bd27c5ccb460baba38f
Milyukov Sergey <serrguk@gmail.om>
2021-08-03 18:18:18 +0700

    Merge remote-tracking branch 'origin/master'

--- 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
@@ -14,15 +14,22 @@ public class Main {
             int indexInput;
             if (input.equals("")) {
                 break;
-            } else if (inputWords[0].matches("[0-9]")) {
-                if (inputWords[1].equals("ADD")) {
+            } else if (inputWords[0].matches("[0-9]") && inputWords[1].equals("ADD")) {
                     indexInput = Integer.parseInt(inputWords[0]);
                     for (int i = 2; i < inputWords.length; i++) {
                         todo = todo.concat(inputWords[i]) + " ";
                     }
                     todoList.add(indexInput, todo.trim());
+                    System.out.println("Добавлено дело " + todo);
+
+            } else if (inputWords[0].equals("ADD")) {
+                for (int i = 1; i < inputWords.length; i++) {
+                    todo = todo.concat(inputWords[i]) + " ";
                 }
-            } else if (inputWords[0].equals("LIST")) {
+                todoList.add(todo.trim());
+                System.out.println("Добавлено дело " + todo);
+            }
+            else if (inputWords[0].equals("LIST")) {
                 int countTodo = 1;
                 for (String inputWord : inputWords) {
                     System.out.println(countTodo + " - " + inputWord);
@@ -30,7 +37,7 @@ public class Main {
                 }
             } else if (inputWords[0].equals("EDIT")) {
                 indexInput = Integer.parseInt(inputWords[1]);
-                String todoOld = inputWords[indexInput];
+                String todoOld = todoList.todos.get(indexInput);
                 for (int i = 2; i < inputWords.length; i++) {
                     todo = todo.concat(inputWords[i]) + " ";
                 }
