3d1c283b5bac5df516b5df2e5d72b85b6ef3d8bf
Milyukov Sergey <serrguk@gmail.om>
2021-08-04 12:00:51 +0700

    Merge remote-tracking branch 'origin/master'

--- 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
@@ -12,6 +12,7 @@ public class Main {
             String[] inputWords = input.split(" ");
             String todo = "";
             int indexInput;
+
             if (input.equals("")) {
                 break;
             } else if (inputWords[0].matches("[0-9]") && inputWords[1].equals("ADD")) {
@@ -20,19 +21,19 @@ public class Main {
                         todo = todo.concat(inputWords[i]) + " ";
                     }
                     todoList.add(indexInput, todo.trim());
-                    System.out.println("Добавлено дело " + todo);
+                    System.out.printf("Добавлено дело \"%s\"\n", todo.trim());
 
             } else if (inputWords[0].equals("ADD")) {
                 for (int i = 1; i < inputWords.length; i++) {
                     todo = todo.concat(inputWords[i]) + " ";
                 }
                 todoList.add(todo.trim());
-                System.out.println("Добавлено дело " + todo);
+                System.out.printf("Добавлено дело \"%s\"\n", todo.trim());
             }
             else if (inputWords[0].equals("LIST")) {
                 int countTodo = 1;
-                for (String inputWord : inputWords) {
-                    System.out.println(countTodo + " - " + inputWord);
+                for (String todoInList : todoList.getTodos()) {
+                    System.out.println(countTodo + " - " + todoInList);
                     countTodo++;
                 }
             } else if (inputWords[0].equals("EDIT")) {
@@ -41,12 +42,12 @@ public class Main {
                 for (int i = 2; i < inputWords.length; i++) {
                     todo = todo.concat(inputWords[i]) + " ";
                 }
-                todoList.edit(todo, indexInput);
-                System.out.println("Дело " + todoOld + "заменено на " + todo);
+                todoList.edit(todo.trim(), indexInput);
+                System.out.println("Дело \"" + todoOld.trim() + "\" заменено на \"" + todo.trim() + "\"");
             } else if (inputWords[0].equals("DELETE")) {
                 indexInput = Integer.parseInt(inputWords[1]);
 
-                System.out.println("Дело" + todoList.todos.get(indexInput) + " удалено");
+                System.out.println("Дело " + todoList.todos.get(indexInput) + " удалено");
                 todoList.delete(indexInput);
             }
         }
