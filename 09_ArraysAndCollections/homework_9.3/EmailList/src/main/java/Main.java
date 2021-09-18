1019f94571a7b8a88d5988bfc7027bcea419c6c8
Milyukov Sergey <serrguk@gmail.om>
2021-09-19 00:23:59 +0700

    EmailList.java modified

--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
@@ -1,7 +1,9 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
+    private static final EmailList emailList = new EmailList();
     
     /* TODO:
         Пример вывода списка Email, после ввода команды LIST в консоль:
@@ -19,15 +21,28 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("0")) {
                 break;
             }
-            
             //TODO: write code here
-            
+            String regex = "\\w+@\\w+\\.[ru]*[com]*";
+            String[] words = input.split(" ");
+
+            if (words[0].equals("ADD") && words.length == 2) {
+                if (words[1].toLowerCase(Locale.ROOT).matches(regex)) {
+                    emailList.add(words[1].toLowerCase(Locale.ROOT));
+                    System.out.println("Email внесён в список"); //Вывод для наглядности
+                } else System.out.println(WRONG_EMAIL_ANSWER);
+            } else if (words[0].equals("LIST")) {
+                if (emailList.emails.size() != 0) {
+                    for (String list : emailList.emails) {
+                        System.out.println(list);
+                    }
+                } else System.out.println("Список пуст");
+            } else System.out.println("Неверный формат ввода");
         }
     }
 }
