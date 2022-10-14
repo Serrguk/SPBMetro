a9c0f1e6fda91df2e648030435b16a97ea3e914e
Sergey Milyukov <serrguk@gmail.com>
2022-10-14 11:46:25 +0700

    Project 04_Encapsulation/Elevator complete

---
+++ 04_Encapsulation/01_Elevator/src/ru/skillbox/Elevator.java
@@ -0,0 +1,45 @@
+package ru.skillbox;
+
+public class Elevator {
+    private int currentFloor = 1;
+    private final int minFloor;
+    private final int maxFloor;
+
+    public Elevator(int minFloor, int maxFloor) {
+        this.minFloor = minFloor;
+        this.maxFloor = maxFloor;
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    private void floorDown() {
+        currentFloor--;
+    }
+
+    private void floorUp() {
+        currentFloor++;
+    }
+
+    public void move(int floor) {
+        if (floor < minFloor || floor > maxFloor) {
+            System.out.println("Данного этажа в здании нет.\n" +
+                    "Лифт двигается в пределах от " + minFloor + " до " + maxFloor + " этажа.");
+        } else if (floor == getCurrentFloor()) {
+            System.out.println("Вы уже на этаже " + currentFloor);
+        } else if (floor > getCurrentFloor()) {
+            while (floor > getCurrentFloor()) {
+                floorUp();
+                System.out.println("Этаж номер " + getCurrentFloor());
+            }
+            System.out.println("Вы прибыли на этаж номер " + getCurrentFloor());
+        } else {
+            while (floor < getCurrentFloor()) {
+                floorDown();
+                System.out.println("Этаж номер " + getCurrentFloor());
+            }
+            System.out.println("Вы прибыли на этаж номер " + getCurrentFloor());
+        }
+    }
+}
--- 04_Encapsulation/01_Elevator/src/ru/skillbox/Main.java
+++ 04_Encapsulation/01_Elevator/src/ru/skillbox/Main.java
@@ -3,5 +3,7 @@ package ru.skillbox;
 public class Main {
 
     public static void main(String[] args) {
+        Elevator elevator = new Elevator(-3, 25);
+        elevator.move(50);
     }
 }
