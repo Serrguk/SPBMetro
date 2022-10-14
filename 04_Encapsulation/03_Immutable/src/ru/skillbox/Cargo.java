22fc2126074dbc35399506edda1662cd391e2751
Sergey Milyukov <serrguk@gmail.com>
2022-10-14 15:22:17 +0700

    Project 04_Encapsulation/Immutable started

--- 04_Encapsulation/03_Immutable/src/ru/skillbox/Cargo.java
+++ 04_Encapsulation/03_Immutable/src/ru/skillbox/Cargo.java
@@ -8,4 +8,36 @@ public class Cargo {
     private String registrationNumber;
     private boolean isFragile;
 
+    public Cargo(Dimensions dimensions, double mass, String addressDestination, boolean isTurn, String registrationNumber, boolean isFragile) {
+        this.dimensions = dimensions;
+        this.mass = mass;
+        this.addressDestination = addressDestination;
+        this.isTurn = isTurn;
+        this.registrationNumber = registrationNumber;
+        this.isFragile = isFragile;
+    }
+
+    public void setDimensions(Dimensions dimensions) {
+        this.dimensions = dimensions;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    public void setAddressDestination(String addressDestination) {
+        this.addressDestination = addressDestination;
+    }
+
+    public void setTurn(boolean turn) {
+        isTurn = turn;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public void setFragile(boolean fragile) {
+        isFragile = fragile;
+    }
 }
