14900df25a7960d8175b072a8d05a918baec6652
Milyukov Sergey <serrguk@gmail.om>
2023-03-25 17:07:38 +0700

    Project created

---
+++ AbstractClasses/practice_1/src/Company.java
@@ -0,0 +1,14 @@
+import Interfaces.Employee;
+
+import java.util.ArrayList;
+
+public class Company {
+
+    public ArrayList<Employee> getTopSalaryStaff(int count) {
+        return null;
+    }
+
+    public ArrayList<Employee> getLowesSalaryStaff(int count) {
+        return null;
+    }
+}
