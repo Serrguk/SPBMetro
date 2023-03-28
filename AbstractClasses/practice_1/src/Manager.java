d3bb280374ec95d504eb549b612f6a2f41ea6626
Milyukov Sergey <serrguk@gmail.om>
2023-03-29 01:41:27 +0700

    Method getMonthSalary implemented

--- AbstractClasses/practice_1/src/Manager.java
+++ AbstractClasses/practice_1/src/Manager.java
@@ -1,10 +1,21 @@
 public class Manager extends Employees {
+
+    private double incomeForCompany;
     public Manager(String name, double salary, Company company) {
         super(name, salary, company);
+        incomeForCompany = 115000 + (140000 - 115000) * Math.random();
     }
 
     @Override
-    public int getMonthSalary() {
-        return 0;
+    public double getMonthSalary() {
+        return Math.round(getSalary() + incomeForCompany * 0.05);
+    }
+
+    public double getIncomeForCompany() {
+        return Math.round(incomeForCompany);
+    }
+
+    public void setIncomeForCompany() {
+        this.incomeForCompany = 115000 + (140000 - 115000) * Math.random();
     }
 }
