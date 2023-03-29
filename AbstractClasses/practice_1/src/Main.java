a23171e64c871b160d75d1583f0530c039a6f003
Milyukov Sergey <serrguk@gmail.om>
2023-03-29 20:53:06 +0700

    main modified

--- AbstractClasses/practice_1/src/Main.java
+++ AbstractClasses/practice_1/src/Main.java
@@ -1,21 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        Company google = new Company(10_000_000D);
-        TopManager ivanPetrov = new TopManager("Иван Петров", 300_000, google);
+        Company google = new Company(1E8);
+        TopManager ivanPetrov = new TopManager("Иван Петров", 3E5, google);
         google.hire(ivanPetrov);
 
         System.out.println(google.getIncome());
         System.out.println(google.showStaff());
 
-        google.hire(new Manager("Тото Кутуньо", 35000, google));
+        google.hire(new Manager("Тото Кутуньо", 3.5E4, google));
         System.out.println(google.showStaff());
-        Manager manager = new Manager("R2D2", 120_000, google);
+        Manager manager = new Manager("R2D2", 12E4, google);
         System.out.println(manager.getIncomeForCompany());
         System.out.println(manager.getMonthSalary());
         System.out.println();
-        TopManager top = new TopManager("Big man", 350_000, google);
-        System.out.println(top.getMonthSalary());
+        TopManager top = new TopManager("Big man", 35E4, google);
 
     }
 }
