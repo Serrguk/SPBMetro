c820795c25f54b553f0b4b9afeec5aa1a5f6de9b
Milyukov Sergey <serrguk@gmail.om>
2023-04-14 00:01:43 +0700

    Method findPlanesLeavingInTheNextTwoHours complete

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,17 +1,25 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
+
+import java.util.Date;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        Airport airport = Airport.getInstance();
+        System.out.println(findPlanesLeavingInTheNextTwoHours(airport));
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        Date now = new Date();
+        Date twoHoursLater = new Date(now.getTime() + (2 * 60 * 60 * 1000));
+
+        return airport.getTerminals().stream()
+                .flatMap(terminal -> terminal.getFlights().stream())
+                .filter(flight -> flight.getType() == Flight.Type.DEPARTURE
+                        && flight.getDate().after(now)
+                        && flight.getDate().before(twoHoursLater))
+                .toList();
     }
 
 }
