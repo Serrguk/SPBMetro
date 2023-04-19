7bdc0de948e5d648304f282d697ba3afa215dfad
Milyukov Sergey <serrguk@gmail.om>
2023-04-20 00:51:30 +0700

    The creation of the metro map has begun O_o

--- ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/main/test/java/TestRouteCalculator.java
+++ ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/main/test/java/TestRouteCalculator.java
@@ -6,20 +6,34 @@ import java.util.List;
 
 public class TestRouteCalculator extends TestCase {
     List<Station> route;
+    StationIndex stationIndex = new StationIndex();
 
     @Override
     protected void setUp() {
-        Line line1 = new Line(1, "First");
-        Line line2 = new Line(2, "Second");
+        //Пример из видео
+        Line line1 = new Line(1, "Первая");
+        Line line2 = new Line(2, "Вторая");
+        Line line3 = new Line(3, "Третья");
 
         route = List.of(new Station("Петровская", line1),
                 new Station("Садовая", line1),
                 new Station("Бредовая", line2),
                 new Station("Столовая", line2));
 
+        // Создание схемы метро
+        stationIndex.addStation(new Station("Петровская", line1));
+        stationIndex.addStation(new Station("Садовая", line1));
+        stationIndex.addStation(new Station("Бредовая", line2));
+        stationIndex.addStation(new Station("Столовая", line2));
+        stationIndex.addStation(new Station("Академическая", line3));
+        stationIndex.addStation(new Station("Политехническая", line3));
+        stationIndex.addLine(line1);
+        stationIndex.addLine(line2);
+        stationIndex.addLine(line3);
+
     }
 
-    public void testCalculateDuration() throws Exception {
+    public void testCalculateDuration() {
         double actual = RouteCalculator.calculateDuration(route);
         double expected = 8.5;
         assertEquals(expected, actual);
